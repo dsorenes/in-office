7e14291d12d773ed1308838c3030d5e33a9706dd
Daniel Sørenes <daniel@sorenes.no>
2022-09-02 13:58:43 +0200

    Working impl of seeing who's at the office

--- in_office/main.py
+++ in_office/main.py
@@ -1,14 +1,42 @@
 import subprocess
+from difflib import SequenceMatcher
 
-people = [
-        "eika",
-        "fredrik",
-        "peter",
-        "knut",
-        "dsr"
-        ]
+import schedule
+
+people = {
+        "eika": "Kenneth",
+        "fredrik": "Fredrik",
+        "peter": "Peter",
+        "knut": "Knut",
+        "dsr": "Daniel"
+        }
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+@schedule.repeat(schedule.every(10).minutes)
+def run_nmap():
+    print("Scanning connected hosts...")
+    subprocess.run("nmap -aP 192.168.1.0/24")
+
+@schedule.repeat(schedule.every(5).seconds)
+def in_the_office():
+    print("Checking who's at the office")
+    connected_hosts = subprocess.run("./find_hosts.sh", universal_newlines=True, stdout=subprocess.PIPE)
+
+    hosts = connected_hosts.stdout.split("\n")
+
+    people_at_the_office = []
+
+    for host in hosts:
+        host_name = host.split(" ")[0][:-4].split("-")[0]
+        for person in people.keys():
+            similarity = similar(person, host_name)
+            if similarity > 0.5:
+                people_at_the_office.append(people.get(person))
+    print(f"people at the office: {people_at_the_office}")
+
 
 if __name__ == "__main__":
-    output = subprocess.check_output("./find_hosts.sh", shell=True)
-
-    print(output.decode("utf-8"))
+    while True:
+        schedule.run_pending()
